1eb862abc3d55c87ae2d9ef8c0b7e66531073cc3
Aakash Verma <aku06_ran@yahoo.co.in>
2018-01-25 10:06:20 +0530

    Added test for valid throw array with throw frame sum less than 10

--- __test__/test.js
+++ __test__/test.js
@@ -4,15 +4,15 @@ describe('Testing input for validation:', () => {
   describe('Testing array for validation', () => {
     it('Testing for min array length of 11', () => {
       const arrayMin = [10, 10, 10, 10, 10, 10, 10, 10, 10, 2, 3];
-      expect(bowling.verifyArr(arrayMin)).toBe(true);
+      expect(bowling.verifyArrLen(arrayMin)).toBe(true);
     });
     it('Testing for max array length of 21', () => {
       const arrayMax = [1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 10, 2, 3];
-      expect(bowling.verifyArr(arrayMax)).toBe(true);
+      expect(bowling.verifyArrLen(arrayMax)).toBe(true);
     });
     it('Testing an input arrayw with valid length', () => {
       const arrayMax = [1, 2, 1, 2, 1, 2, 1, 2, 6, 4, 10, 1, 2, 1, 2, 1, 2, 10, 2, 3];
-      expect(bowling.verifyArr(arrayMax)).toBe(true);
+      expect(bowling.verifyArrLen(arrayMax)).toBe(true);
     });
   });
   describe('Testing array elements for validity', () => {
@@ -27,6 +27,12 @@ describe('Testing input for validation:', () => {
     it('Testing a valid element', () => {
       const minElem = 3;
       expect(bowling.verifyNum(minElem)).toBe(true);
+    });
+  });
+  describe('Testing validity of throw frames', () => {
+    it('Testing valid throw frames with sum less than 10', () => {
+      const throwFrame = [3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6];
+      expect(bowling.verifyArr(throwFrame)).toBe(true);
     });
   });
 });
--- bowling.js
+++ bowling.js
@@ -1,9 +1,25 @@
 // Length of array must be greater than or equal to 11 and less than 22
-const verifyArr = arr => arr.length >= 11 && arr.length <= 21;
+const verifyArrLen = arr => arr.length >= 11 && arr.length <= 21;
 
 // Value of throw should be greater than or equal to 0 and less than 11
 const verifyNum = num => num >= 0 && num <= 10;
 
+// Checking for throw frames for sum >= 0 and <10
+const verifyArr = function verifyArr(arr) {
+  if (arr.every(elem => verifyNum(elem))) {
+    let prev = 0;
+    let curr = 1;
+    const len = arr.length;
+    for (; prev < len;) {
+      if (arr[prev] + arr[curr] < 10) {
+        prev += 2;
+        curr = prev + 1;
+      }
+    }
+    return true;
+  }
+  return false;
+};
 module.exports = {
-  verifyArr, verifyNum,
+  verifyArrLen, verifyNum, verifyArr,
 };
