a2b6e796f758c2c9a4c5344c1e9d9180b9a93666
Aakash Verma <aku06_ran@yahoo.co.in>
2018-01-25 12:28:36 +0530

    Added test for mix of spares and trivials

--- __test__/test.js
+++ __test__/test.js
@@ -140,5 +140,9 @@ describe('Testing input for validation:', () => {
       const trivialArr = [4, 6, 4, 6, 4, 6, 4, 6, 4, 6, 4, 6, 4, 6, 4, 6, 4, 6, 4];
       expect(bowling.score(trivialArr)).toBe(-1);
     });
+    it('Testing sum of mix of valid spares and trivials', () => {
+      const trivialArr = [3, 7, 4, 6, 4, 2, 4, 2, 4, 2, 4, 0, 4, 0, 4, 0, 0, 6, 4, 6, 1];
+      expect(bowling.score(trivialArr)).toBe(75);
+    });
   });
 });
