6f430d539ccd324a196c2c43a25a275b9982c425
Aakash Verma <aku06_ran@yahoo.co.in>
2018-01-25 11:03:19 +0530

    Added comments

--- bowling.js
+++ bowling.js
@@ -6,22 +6,27 @@ const verifyNum = num => num >= 0 && num <= 10;
 
 // Checking for throw frames for sum >= 0 and <10
 const verifyArr = function verifyArr(arr) {
+  // testing if elements are valid before testing frames
   if (arr.every(elem => verifyNum(elem))) {
     let prev = 0;
     let noOfFrames = 0;
     const len = arr.length;
+    // Checking only for 10 frames here
     for (; noOfFrames < 10;) {
+      // for frame sum less than 10
       if (arr[prev] + arr[prev + 1] < 10) {
         prev += 2;
-      } else if (arr[prev] === 10) {
+      } else if (arr[prev] === 10) { // tesing for strike frame
         prev += 1;
-      } else if (arr[prev] + arr[prev + 1] === 10) {
+      } else if (arr[prev] + arr[prev + 1] === 10) { // testing for spare frame
         prev += 2;
       } else {
         return false;
       }
       noOfFrames += 1;
     }
+    // Testing there's an 11th frame with valid length
+    // only if there's a spare or strike in the 10th frame
     if ((arr[prev - 1] === 10 && len - prev === 2) || (arr[prev - 1] !== 10 && (prev === len
       || len - prev === 1))) {
       return true;
